e37324bd6c54a9c385cceb7a2c9fbf0db7e40785
albertgoncalves <goncalves.albert@gmail.com>
2019-04-18 00:46:42 -0400

    very nice

--- src/spline.py
+++ src/spline.py
@@ -11,18 +11,17 @@ def interpolate(t, degree, points):
     n = len(points)
     if (degree < 1) or (degree >= n):
         raise ValueError("0 < degree < n points")
-    d = len(points[0])
     knots = list(range(n + degree + 1))
-    weights = [1] * n
     domain = [degree, len(knots) - 1 - degree]
-    low = knots[domain[0]]
-    high = knots[domain[1]]
+    low, high = [knots[domain[i]] for i in range(2)]
     t = t * (high - low) + low
     if (t < low) or (t > high):
         raise ValueError("t out of bounds")
+    weights = [1] * n
     for s in range(*domain):
         if (t >= knots[s]) and (t <= knots[s + 1]):
             break
+    d = len(points[0])
     f = lambda i, j: points[i][j] * weights[i] if j != d else weights[i]
     v = [[f(i, j) for j in range(d + 1)] for i in range(n)]
     for l in range(1, degree + 1):
@@ -30,8 +29,13 @@ def interpolate(t, degree, points):
             alpha = (t - knots[i]) / (knots[i + degree + 1 - l] - knots[i])
             for j in range(d + 1):
                 v[i][j] = (1 - alpha) * v[i - 1][j] + alpha * v[i][j]
-    result = [v[s][i] / v[s][d] for i in range(d)]
-    return result
+    return [v[s][i] / v[s][d] for i in range(d)]
+
+
+def spline(t, degree, points):
+    return map( lambda t: interpolate(t, degree, points)
+              , map(lambda x: x / t, range(0, t + 1, 1))
+              )
 
 
 def transpose(xs):
@@ -42,31 +46,26 @@ def plot(xs, ys):
     _, ax = subplots()
     ax.scatter(*transpose(xs))
     ax.plot(*transpose(ys))
+    ax.set_aspect("equal")
     tight_layout()
     savefig("{}/pngs/plot.png".format(environ["WD"]))
     close()
 
 
-def spline(t, degree, points):
-    return map( lambda t: interpolate(t, degree, points)
-              , map(lambda x: x / t, range(0, t + 1, 1))
-              )
-
-
 def main():
-    t = 100
-    degree = 2
     points = \
         [ [-0.5, 3]
-        , [-1, 0]
+        , [-2, 0]
         , [0.5, -0.5]
         , [0, 2]
-        , [1, 0]
+        , [3.5, 0]
         , [-0.5, 0.5]
-        , [-1, 0]
-        , [-1, -1]
+        , [-2, 0]
+        , [-3, -1]
+        , [2, -0.5]
+        , [0, -2.75]
         ]
-    plot(points, spline(t, degree, points))
+    plot(points, spline(150, 2, points))
 
 
 if __name__ == "__main__":
