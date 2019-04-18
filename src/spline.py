433f67cab70648a441b914e94dc3eff80c40e617
albertgoncalves <goncalves.albert@gmail.com>
2019-04-18 19:00:36 -0400

    still works?

--- src/spline.py
+++ src/spline.py
@@ -27,8 +27,10 @@ def interpolate(points):
         for l in range(1, degree + 1):
             for i in range(s, (s - degree - 1 + l), -1):
                 alpha = (t - knots[i]) / (knots[i + degree + 1 - l] - knots[i])
-                for j in range(d + 1):
-                    v[i][j] = (1 - alpha) * v[i - 1][j] + alpha * v[i][j]
+                v[i] = \
+                    [ (1 - alpha) * v[i - 1][j] + alpha * v[i][j]
+                      for j in range(d + 1)
+                    ]
         return [v[s][i] / v[s][d] for i in range(d)]
     return f
 
