8ab5ffefc57f7521db598a56fe585e6c9e7435ad
albertgoncalves <goncalves.albert@gmail.com>
2019-04-20 23:12:36 -0400

    ./main 13 pngs/ml.png && open pngs/ml.png

--- src/main.ml
+++ src/main.ml
@@ -4,12 +4,27 @@ module R = Random
 module S = Spline
 module Y = Sys
 
+let coordinate (min : float) (max : float) : float =
+    (R.float (max -. min)) +. min
+
+let points (n : int) (xmin : float) (xmax : float) (ymin : float)
+        (ymax : float) : float list list
+    = L.init n (fun _ -> [coordinate xmin xmax; coordinate ymin ymax])
+
+let sentences (n : int) (i : int) (j : int) : float list list list =
+    L.init i (fun i' ->
+        L.concat (L.init j (fun j' ->
+            let i' = float_of_int i' in
+            let j' = float_of_int j' in
+            points (R.int n) j' (j' +. 1.0) i' (i' +. 1.0))))
+
 let main () : unit =
-    let j = 1 in
-    let i = 1 in
-    let points =
+    let j = 8 in
+    let i = 10 in
+    let resolution = 500 / (min i j) in
+    let points' =
         Y.argv.(1) |> int_of_string |> R.init
-        ; L.init (R.int 15) (fun _ -> L.init 2 (fun _ -> R.float 1.0)) in
+        ; sentences 5 i j in
     let black : D.color =
         { r = 0.0
         ; g = 0.0
@@ -20,14 +35,17 @@ let main () : unit =
         ; g = 1.0
         ; b = 1.0
         } in
-    let linewidth = 0.002 in
-    let radius = 0.035 in
-    let (surface, context, dimensions) = D.initialize 1000 i j in
+    let linewidth = 0.01 in
+    let radius = 0.1 in
+    let (surface, context, dimensions) = D.initialize resolution i j in
     D.antialias context
     ; D.background context dimensions white
     ; D.scale context dimensions
-    ; D.dots context points (linewidth /. 4.0) radius black
-    ; D.lines context (S.spline points 100) linewidth black
+    ; L.iter
+        (fun points'' ->
+            D.dots context points'' (linewidth /. 5.0) radius black
+            ; D.lines context (S.spline points'' 100) linewidth black)
+        points'
     ; D.export surface Y.argv.(2)
 
 let () = main ()
